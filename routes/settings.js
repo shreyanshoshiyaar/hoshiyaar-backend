23f1608377540b93551790d8ebaf6e7458aba9c5
akshitravula <143444457+akshitravula@users.noreply.github.com>
2026-05-13 14:38:07 +0530

    Pushing backend code to new repository

--- routes/settings.js
+++ routes/settings.js
@@ -26,6 +26,19 @@ router.get('/:key', async (req, res) => {
         description: "URL for the 'Today's Mission' video on the homescreen"
       });
     }
+    if (!setting && key === 'homepage_slides') {
+      setting = await SystemSettings.create({
+        key: 'homepage_slides',
+        value: [
+          "https://res.cloudinary.com/dcxlzfyfp/image/upload/v1778578700/img-to-link/wunxaopn4qfirxnfdwa6.webp",
+          "https://res.cloudinary.com/dcxlzfyfp/image/upload/v1778567489/img-to-link/n9fo2moxpmfwyp04ueob.webp",
+          "https://res.cloudinary.com/dcxlzfyfp/image/upload/v1778567487/img-to-link/zy3aaizcfjl5qwm9ewjx.webp",
+          "https://res.cloudinary.com/dcxlzfyfp/image/upload/v1778579285/img-to-link/eqdx0kyjrhkruh0ownxd.webp",
+          "https://res.cloudinary.com/dcxlzfyfp/image/upload/v1778578699/img-to-link/tynaqnlzmbvoaafwu3do.webp"
+        ],
+        description: "List of 5 image URLs for the mobile homepage carousel"
+      });
+    }
     return res.json(setting);
   } catch (err) {
     res.status(500).json({ error: err.message });
